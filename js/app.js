68048df44e66aeaba6b238cf31ff391d0ef39303
Francisco <ramirezcrisantofranciscojavier@gmail.com>
2021-10-05 21:46:37 -0500

    Se creo dos metodos dentro del objeto Calculadora, el primero para agregar todas funcionalidades de la calcula, mientras que el segundo es un ciclo el cual permite agregarles a todos los botones o teclas dos eventos, el primer evento que sirve para modificar el tamaño de estos al momento de dejar apretada una tecla, el segundo evento es para dejar de modificar el tamaño al momento de solta dicha tecla o boton

--- js/app.js
+++ js/app.js
@@ -21,5 +21,39 @@ var Calculadora = {
   teclas: document.getElementsByClassName("tecla"),
   operandoA: 0,
   operandoB: 0,
-  operacion: ""
+  operacion: "",
+
+  evento: function(){
+    this.uno.onclick=this.eventUno
+    this.dos.onclick=this.eventDos
+    this.tres.onclick=this.eventTres
+    this.cuatro.onclick=this.eventCuatro
+    this.cinco.onclick=this.eventCinco
+    this.seis.onclick=this.eventSeis
+    this.siete.onclick=this.eventSiete
+    this.ocho.onclick=this.eventOcho
+    this.nueve.onclick=this.eventNueve
+    this.cero.onclick=this.eventCero
+
+    this.on.onclick=this.eventOn
+    this.sign.onclick=this.eventSign
+    this.punto.onclick=this.eventPunto
+    this.igual.onclick=this.eventIgual
+    this.mas.onclick=this.eventMas
+    this.menos.onclick=this.eventMenos
+    this.por.onclick=this.eventPor
+    this.dividido.onclick=this.eventDividido
+    this.tamaño()
+  },
+  tamaño: function(){
+    for(let i = 0; i < this.teclas.length; i++){
+        Calculadora.teclas[i].onmousedown=function(){
+          Calculadora.teclas[i].style.transform="perspective(100px) translatez(-0.5vw)"
+        }
+        Calculadora.teclas[i].onmouseup=function(){
+          Calculadora.teclas[i].style.transform="none"
+        }
+    }
+  }
 }
+Calculadora.tamaño()
